8df7db7c2f4060b2d67315b2daa918b2bf89bce8
samade747 <samad.e747@gmail.com>
2024-03-11 15:10:19 +0500

    309pm 11 3 2024

---
+++ samad/tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
